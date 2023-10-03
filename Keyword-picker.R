4dd1c50309512c3867e9014a3f9e3c7cb6d8b944
LizzieAugarde <129846795+LizzieAugarde@users.noreply.github.com>
2023-10-03 12:13:32 +0100

    Converted Keyword-picker file to R

---
+++ Keyword-picker.R
@@ -0,0 +1,6 @@
+###Learning web development keywords
+#03/10/2023 - created
+
+words <- c("Agile", "Algorithm", "API", "Application", "Adaptive design", "Boostrap", "Backend", "Browser", "Bug", "Cache", "Code", "CSS", "Data structure", "Debugging", "Deployment")
+
+sample(words, 1, replace = FALSE, prob = NULL)
